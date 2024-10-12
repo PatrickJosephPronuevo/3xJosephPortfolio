d0a9e8014f12d10197b8e841a9959bc1d78e33c0
Patrick Joseph Pronuevo <pronuevopatrickjosepha4977@gmail.com>
2024-10-12 16:23:02 +0800

    added new resume

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,4 +1,4 @@
-import Resume from "../assets/docs/Patrick Joseph Pronuevo - CV NO PIC.pdf";
+import Resume from "../assets/docs/Patrick_JosephPronuevo_Resume.pdf";
 
 const DownloadButton = () => {
   const buttonStyle =
