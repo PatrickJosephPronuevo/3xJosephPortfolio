909f5b8c19faf86e931dd941b36feb0f4a99cb2f
Patrick Joseph Pronuevo <pronuevopatrickjosepha4977@gmail.com>
2024-11-28 03:10:40 +0800

    Update on project link

--- src/data/ProjectsData.jsx
+++ src/data/ProjectsData.jsx
@@ -45,7 +45,7 @@ export const ProjectsData = [
     tools: ["C#", "ShaderLab", "HLSL", "UNTIY"],
     url: "https://github.com/3xjoseph/GOL-main",
     play: null, // Example play link
-    install: "https://www.mediafire.com/file/h01096opdtwsq29/Guardians_of_Luzviminda.apk/file", // Example install link
+    install: "https://www.mediafire.com/file/p7mrds0p7zwnpnm/GOL.apk/file", // Example install link
   },
   {
     img: WhisperoftheDead,
